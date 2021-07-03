1ecde8d48b8d1c85713899bad47ae5beebee8535
BeatsLeo <1059899674@qq.com>
2021-07-03 14:54:29 +0800

    完善功能文档

--- shopping_card_manage_system/consumer.h
+++ shopping_card_manage_system/consumer.h
@@ -7,8 +7,8 @@ typedef struct {
 	char name_[10];	//姓名
 	char password_[6];	//密码
 	char id_[18];	//身份证
-	float balance_;	//余额
-	int score_;	//积分
+	float balance_ = 0;	//余额
+	int score_ = 0;	//积分
 	bool is_available_;	//激活状态
 
 	Consumer_Node* next_ = NULL;	//下一个指针
@@ -20,8 +20,8 @@ typedef struct {
 	char name_[10];	//姓名
 	char password_[6];	//密码
 	char id_[18];	//身份证
-	float balance_;	//余额
-	int score_;	//积分
+	float balance_ = 0;	//余额
+	int score_ = 0;	//积分
 	bool is_available_;	//激活状态
 }Consumer;
 
