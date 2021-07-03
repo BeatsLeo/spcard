a4c4025ccf6c31d45b91c3aa615fc4d6903820c3
BeatsLeo <1059899674@qq.com>
2021-07-03 09:55:19 +0800

    let's go

---
+++ shopping_card_manage_system/administor.h
@@ -0,0 +1,32 @@
+﻿#ifndef ADMINISTOR_H
+#define ADMINISTOR_H
+#include"consumer.h"
+//管理员结构体信息
+typedef struct
+{
+	int account_;	//账号
+	char password_[6];	//密码
+	Consumer_Node* consumer_;	//消费者链表
+
+	Administor_Node* next_;	//下一个节点位置
+
+}Administor_Node;
+
+void ShowWindow();	//菜单窗口
+void CheckApply();	//检查申请信息，并审批申请信息
+void MakeCard();	//制卡
+void SendCard();	//发卡
+void ShowInfo();	//查看信息
+void ChangePsw();	//修改个人密码
+void Quit();		//退出
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ shopping_card_manage_system/menu.h
@@ -0,0 +1,22 @@
+﻿#ifndef MENU_H
+#define MENU_H
+#include<stdio.h>
+#include"consumer.h"
+#include"administor.h"
+typedef struct {
+	Administor_Node* admin_;	//管理员链表
+	Consumer_Node* consumer_;	//消费者链表
+}Master;
+
+typedef struct {
+	int number_;	//卡号
+	char name_[10];	//姓名
+	char password_[6];	//密码
+	char id_[18];	//身份证
+}Apply;
+
+int ShowWindow();	//菜单窗口
+void MainOperator(int n);	//主操作页面
+Apply* AskCard();	//申请购物卡
+
+#endif
